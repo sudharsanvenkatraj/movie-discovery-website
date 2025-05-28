dee0f45c1ff8e8a2981ecaa5b09fa0aa8a93fdb2
sudharsan <writetosudharsn@gmail.com>
2025-05-28 15:27:41 +0530

    created mock id serach

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -1,6 +1,15 @@
 import { RenderMode, ServerRoute } from '@angular/ssr';
+import { routesIDs } from './shared/mocks/routes-ids';
 
 export const serverRoutes: ServerRoute[] = [
+  {
+   path: 'details/:id',
+   renderMode: RenderMode.Prerender,
+   async getPrerenderParams() {
+      const ids = routesIDs;
+      return ids.map(id => ({ id }));
+  },
+ },
   {
     path: '**',
     renderMode: RenderMode.Prerender
